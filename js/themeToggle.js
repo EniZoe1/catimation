3d3b7292d827b0e3c876da7d16153457761248ec
Rafal <rafalszkatula1@gmail.com>
2023-06-19 17:13:41 +0200

    js corrects

--- js/themeToggle.js
+++ js/themeToggle.js
@@ -1,8 +1,7 @@
-let body = document.querySelector("body");
-let themeToggleButton = document.querySelector(".theme-toggle");
-let circle = document.querySelector(".fa-circle");
-let themeToggleIcon = document.querySelector(".theme-toggle__icon");
-
+const body = document.querySelector("body");
+const themeToggleButton = document.querySelector("button");
+const circle = document.querySelector(".fa-circle");
+const themeToggleIcon = document.querySelector(".theme-toggle__icon");
 function themeToggle() {
 	circle.classList.toggle("active");
 	circle.classList.toggle("active");
@@ -12,5 +11,5 @@ function themeToggle() {
 		body.setAttribute("data-mode", "light");
 	else body.setAttribute("data-mode", "dark");
 }
-// themeToggleButton.addEventListener("click", "themeToggle");
+themeToggleButton.addEventListener("click", themeToggle);
 // circle.addEventListener('mouseotu')
